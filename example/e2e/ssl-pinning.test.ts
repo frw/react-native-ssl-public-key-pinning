110b75664f492968009a98fa91907173ef60fd3c
Frederick Widjaja <widjaja.frederick@gmail.com>
2023-04-03 23:01:10 +0700

    test: add e2e tests for initializing and disabling pinning

--- example/e2e/ssl-pinning.test.ts
+++ example/e2e/ssl-pinning.test.ts
@@ -3,19 +3,114 @@ import { by, device, expect, element } from 'detox';
 describe('SSL Pinning', () => {
   beforeEach(async () => {
     await device.launchApp({ newInstance: true });
+
+    await expect(element(by.id('SslPinningAvailability'))).toHaveText(
+      'SSL Pinning Available: YES'
+    );
   });
 
-  it('should succeed request with valid pinning configuration', async () => {
+  it('should succeed fetch with valid pins', async () => {
     await element(by.id('ValidExample')).tap();
-    await element(by.id('TestPinning')).tap();
-    await expect(element(by.id('ResultOutput'))).toHaveText('✅ Status: 200');
+
+    await element(by.id('InitializePinning')).tap();
+    await expect(element(by.id('InitializeResultOutput'))).toHaveText(
+      '✅ Success Initializing'
+    );
+
+    await element(by.id('TestFetch')).tap();
+    await expect(element(by.id('FetchResultOutput'))).toHaveText(
+      '✅ Status: 200'
+    );
   });
 
-  it('should fail request with invalid pinning configuration', async () => {
+  it('should fail fetch with invalid pins', async () => {
     await element(by.id('InvalidExample')).tap();
-    await element(by.id('TestPinning')).tap();
-    await expect(element(by.id('ResultOutput'))).toHaveText(
+
+    await element(by.id('InitializePinning')).tap();
+    await expect(element(by.id('InitializeResultOutput'))).toHaveText(
+      '✅ Success Initializing'
+    );
+
+    await element(by.id('TestFetch')).tap();
+    await expect(element(by.id('FetchResultOutput'))).toHaveText(
       '❌ TypeError: Network request failed'
+    );
+  });
+
+  it('should succeed fetch with unrelated pins', async () => {
+    await element(by.id('InvalidExample')).tap();
+
+    await element(by.id('InitializePinning')).tap();
+    await expect(element(by.id('InitializeResultOutput'))).toHaveText(
+      '✅ Success Initializing'
+    );
+
+    await element(by.id('TestDomainInput')).replaceText('example.com');
+
+    await element(by.id('TestFetch')).tap();
+    await expect(element(by.id('FetchResultOutput'))).toHaveText(
+      '✅ Status: 200'
+    );
+  });
+
+  it('should succeed fetch after initializing with invalid pins and then initializing with valid pins', async () => {
+    await element(by.id('InvalidExample')).tap();
+
+    await element(by.id('InitializePinning')).tap();
+    await expect(element(by.id('InitializeResultOutput'))).toHaveText(
+      '✅ Success Initializing'
+    );
+
+    await element(by.id('ValidExample')).tap();
+
+    await element(by.id('InitializePinning')).tap();
+    await expect(element(by.id('InitializeResultOutput'))).toHaveText(
+      '✅ Success Initializing'
+    );
+
+    await element(by.id('TestFetch')).tap();
+    await expect(element(by.id('FetchResultOutput'))).toHaveText(
+      '✅ Status: 200'
+    );
+  });
+
+  it('should fail fetch after initializing with valid pins and then initializing with invalid pins', async () => {
+    await element(by.id('ValidExample')).tap();
+
+    await element(by.id('InitializePinning')).tap();
+    await expect(element(by.id('InitializeResultOutput'))).toHaveText(
+      '✅ Success Initializing'
+    );
+
+    await element(by.id('InvalidExample')).tap();
+
+    await element(by.id('InitializePinning')).tap();
+    await expect(element(by.id('InitializeResultOutput'))).toHaveText(
+      '✅ Success Initializing'
+    );
+
+    await element(by.id('TestFetch')).tap();
+    await expect(element(by.id('FetchResultOutput'))).toHaveText(
+      '❌ TypeError: Network request failed'
+    );
+  });
+
+  it('should succeed fetch after initializing invalid pins and then disabling it', async () => {
+    await element(by.id('InvalidExample')).tap();
+
+    await element(by.id('InitializePinning')).tap();
+    await expect(element(by.id('InitializeResultOutput'))).toHaveText(
+      '✅ Success Initializing'
+    );
+
+    await element(by.id('DisablePinning')).tap();
+    await expect(element(by.id('InitializeResultOutput'))).toHaveText(
+      '✅ Success Disabling'
+    );
+
+    await element(by.id('TestFetch')).tap();
+    await expect(element(by.id('FetchResultOutput'))).toHaveText(
+      '✅ Status: 200'
     );
   });
 });
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -7,8 +7,14 @@ import {
   TextInput,
   Button,
   ScrollView,
+  KeyboardAvoidingView,
+  Platform,
 } from 'react-native';
-import { initializeSslPinning } from 'react-native-ssl-public-key-pinning';
+import {
+  disableSslPinning,
+  initializeSslPinning,
+  isSslPinningAvailable,
+} from 'react-native-ssl-public-key-pinning';
 
 const GOOGLE_DOMAIN = 'google.com';
 
@@ -27,22 +33,37 @@ const LE_HASHES = [
   'diGVwiVYbubAI3RW4hB9xU8e/CH2GnkuvVFZE8zmgzI=', // ISRG Root X2
 ].join('\n');
 
+const DOMAIN_PLACEHOLDER = 'example.com';
+const PUBLIC_KEY_HASHES_PLACEHOLDER = [
+  'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=',
+  'BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB=',
+].join('\n');
+
 export default function App() {
   const [domain, setDomain] = React.useState('');
   const [publicKeyHashes, setPublicKeyHashes] = React.useState('');
-  const [result, setResult] = React.useState('');
+  const [initializeResult, setInitializeResult] = React.useState('');
+
+  const [testDomain, setTestDomain] = React.useState('');
+  const [fetchResult, setFetchResult] = React.useState('');
 
   const onFillValid = React.useCallback(() => {
     setDomain(GOOGLE_DOMAIN);
     setPublicKeyHashes(GTS_HASHES);
+    setTestDomain(GOOGLE_DOMAIN);
+    setInitializeResult('');
+    setFetchResult('');
   }, []);
 
   const onFillInvalid = React.useCallback(() => {
     setDomain(GOOGLE_DOMAIN);
     setPublicKeyHashes(LE_HASHES);
+    setTestDomain(GOOGLE_DOMAIN);
+    setInitializeResult('');
+    setFetchResult('');
   }, []);
 
-  const onSubmit = React.useCallback(async () => {
+  const onInitializePinning = React.useCallback(async () => {
     try {
       await initializeSslPinning({
         [domain]: {
@@ -50,83 +71,171 @@ export default function App() {
           publicKeyHashes: publicKeyHashes.trim().split('\n'),
         },
       });
-
-      const response = await fetch(`https://${domain}`);
-      setResult(`${response.ok ? '✅' : '❌'} Status: ${response.status}`);
+      setInitializeResult(`✅ Success Initializing`);
+      setFetchResult('');
     } catch (e) {
-      setResult(`❌ ${e}`);
+      setInitializeResult(`❌ ${e}`);
     }
   }, [domain, publicKeyHashes]);
 
+  const onDisablePinning = React.useCallback(async () => {
+    try {
+      await disableSslPinning();
+      setInitializeResult(`✅ Success Disabling`);
+      setFetchResult('');
+    } catch (e) {
+      setInitializeResult(`❌ ${e}`);
+    }
+  }, []);
+
+  const onFetch = React.useCallback(async () => {
+    try {
+      const response = await fetch(`https://${testDomain}`);
+      setFetchResult(`${response.ok ? '✅' : '❌'} Status: ${response.status}`);
+    } catch (e) {
+      setFetchResult(`❌ ${e}`);
+    }
+  }, [testDomain]);
+
   return (
-    <ScrollView
-      style={styles.scrollView}
-      contentContainerStyle={styles.container}
-      keyboardShouldPersistTaps="handled"
-      alwaysBounceVertical={false}
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={styles.root}
     >
-      <View style={styles.field}>
-        <Text style={styles.fieldLabel}>Domain:</Text>
-        <TextInput
-          style={styles.fieldInput}
-          keyboardType="url"
-          inputMode="url"
-          value={domain}
-          onChangeText={setDomain}
-        />
-      </View>
+      <ScrollView
+        style={styles.scrollView}
+        contentContainerStyle={styles.container}
+        keyboardShouldPersistTaps="handled"
+        alwaysBounceVertical={false}
+      >
+        <Text testID="SslPinningAvailability" style={styles.availability}>
+          SSL Pinning Available:{' '}
+          <Text style={styles.availabilityStatus}>
+            {isSslPinningAvailable() ? 'YES' : 'NO'}
+          </Text>
+        </Text>
 
-      <View style={styles.field}>
-        <Text style={styles.fieldLabel}>Public Key Hashes:</Text>
-        <TextInput
-          style={[styles.fieldInput, styles.fieldInputMultiline]}
-          numberOfLines={5}
-          multiline
-          value={publicKeyHashes}
-          onChangeText={setPublicKeyHashes}
-        />
-      </View>
+        <View style={styles.divider} />
 
-      <View style={styles.buttonContainer}>
-        <View style={styles.button}>
-          <Button
-            testID="ValidExample"
-            title="Valid Example"
-            color="green"
-            onPress={onFillValid}
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button
+              testID="ValidExample"
+              title="Valid Example"
+              color="green"
+              onPress={onFillValid}
+            />
+          </View>
+          <View style={[styles.button, styles.componentMarginLeft]}>
+            <Button
+              testID="InvalidExample"
+              title="Invalid Example"
+              color="red"
+              onPress={onFillInvalid}
+            />
+          </View>
+        </View>
+
+        <View style={styles.field}>
+          <Text style={styles.fieldLabel}>Domain:</Text>
+          <TextInput
+            style={styles.fieldInput}
+            keyboardType="url"
+            inputMode="url"
+            placeholder={DOMAIN_PLACEHOLDER}
+            placeholderTextColor="lightgray"
+            value={domain}
+            onChangeText={setDomain}
           />
         </View>
-        <View style={styles.button}>
-          <Button
-            testID="InvalidExample"
-            title="Invalid Example"
-            color="red"
-            onPress={onFillInvalid}
-          />
-        </View>
-        <View style={styles.button}>
-          <Button
-            testID="TestPinning"
-            title="Test Pinning"
-            color="deepskyblue"
-            onPress={onSubmit}
-          />
-        </View>
-      </View>
 
-      <Text testID="ResultOutput" style={styles.result}>
-        {result}
-      </Text>
-    </ScrollView>
+        <View style={styles.field}>
+          <Text style={styles.fieldLabel}>Public Key Hashes:</Text>
+          <TextInput
+            style={[styles.fieldInput, styles.fieldInputMultiline]}
+            numberOfLines={3}
+            multiline
+            placeholder={PUBLIC_KEY_HASHES_PLACEHOLDER}
+            placeholderTextColor="lightgray"
+            value={publicKeyHashes}
+            onChangeText={setPublicKeyHashes}
+          />
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button
+              testID="InitializePinning"
+              title="Initialize Pinning"
+              color="deepskyblue"
+              onPress={onInitializePinning}
+            />
+          </View>
+          <View style={[styles.button, styles.componentMarginLeft]}>
+            <Button
+              testID="DisablePinning"
+              title="Disable Pinning"
+              color="deepskyblue"
+              onPress={onDisablePinning}
+            />
+          </View>
+        </View>
+
+        <Text testID="InitializeResultOutput" style={styles.result}>
+          {initializeResult}
+        </Text>
+
+        <View style={styles.divider} />
+
+        <View style={styles.field}>
+          <Text style={styles.fieldLabel}>Test:</Text>
+          <View style={styles.testInputContainer}>
+            <View style={styles.fetchDomainInput}>
+              <TextInput
+                testID="TestDomainInput"
+                style={styles.fieldInput}
+                keyboardType="url"
+                inputMode="url"
+                placeholder={DOMAIN_PLACEHOLDER}
+                placeholderTextColor="lightgray"
+                value={testDomain}
+                onChangeText={setTestDomain}
+              />
+            </View>
+            <View style={styles.componentMarginLeft}>
+              <Button
+                testID="TestFetch"
+                title="Fetch"
+                color="deepskyblue"
+                onPress={onFetch}
+              />
+            </View>
+          </View>
+        </View>
+
+        <Text testID="FetchResultOutput" style={styles.result}>
+          {fetchResult}
+        </Text>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
   scrollView: {
     backgroundColor: 'darkslategray',
   },
+  availability: {
+    color: 'white',
+  },
+  availabilityStatus: {
+    fontWeight: 'bold',
+  },
   container: {
-    flex: 1,
+    flexGrow: 1,
     alignItems: 'center',
     justifyContent: 'center',
     padding: 24,
@@ -147,18 +256,35 @@ const styles = StyleSheet.create({
   },
   fieldInputMultiline: {
     textAlignVertical: 'top',
+    height: 64,
   },
   buttonContainer: {
     flexDirection: 'row',
     alignItems: 'stretch',
+    marginTop: 8,
   },
   button: {
     flex: 1,
-    margin: 8,
+  },
+  testInputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  fetchDomainInput: {
+    flexGrow: 1,
   },
   result: {
     textAlign: 'center',
     marginVertical: 8,
     color: 'white',
+  },
+  divider: {
+    alignSelf: 'stretch',
+    height: 1,
+    backgroundColor: 'gray',
+    marginVertical: 12,
+  },
+  componentMarginLeft: {
+    marginLeft: 8,
   },
 });
