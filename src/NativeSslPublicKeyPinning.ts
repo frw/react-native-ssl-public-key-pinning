1798ea67e368b5362a80409e31f857fffee1a525
Frederick Widjaja <widjaja.frederick@gmail.com>
2024-04-01 11:24:33 +0700

    fix: new architecture js spec types

--- src/NativeSslPublicKeyPinning.ts
+++ src/NativeSslPublicKeyPinning.ts
@@ -1,13 +1,13 @@
 import { TurboModuleRegistry, type TurboModule } from 'react-native';
 
-import type { PinningOptions } from './types';
-
 export interface Spec extends TurboModule {
-  initialize(options: PinningOptions): Promise<void>;
+  initialize(options: {}): Promise<void>;
   disable(): Promise<void>;
 
   addListener: (eventType: string) => void;
   removeListeners: (count: number) => void;
 }
 
-export default TurboModuleRegistry.getEnforcing<Spec>('SslPublicKeyPinning');
+export default TurboModuleRegistry.getEnforcing<Spec>(
+  'SslPublicKeyPinning'
+) as Spec | null;
